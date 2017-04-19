2263d05ad0ff596b66ce154202ddc90e8aa7a734
Christoffer <jeghedderchristoffer@gmail.com>
2017-04-19 08:34:35 +0200

    Add project files.

---
+++ KanbanBoard/Services/MessageServices.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace KanbanBoard.Services
+{
+    // This class is used by the application to send Email and SMS
+    // when you turn on two-factor authentication in ASP.NET Identity.
+    // For more details see this link https://go.microsoft.com/fwlink/?LinkID=532713
+    public class AuthMessageSender : IEmailSender, ISmsSender
+    {
+        public AuthMessageSender(IOptions<SMSoption> optionsAccessor)
+        {
+            Options = optionsAccessor.Value;
+        }
+
+        public SMSoption Options { get; }  // set only via Secret Manager
+
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            // Plug in your email service here to send an email.
+            return Task.FromResult(0);
+        }
+
+        public Task SendSmsAsync(string number, string message)
+        {
+            // Plug in your SMS service here to send a text message.
+            // Your Account SID from twilio.com/console
+            var accountSid = Options.accountSid;
+            // Your Auth Token from twilio.com/console
+            var authToken = Options.authToken;
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var msg = MessageResource.Create(
+              to: new PhoneNumber(number),
+              from: new PhoneNumber("+12566774377"),
+              body: message);
+
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ KanbanBoard/Services/SMSoption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KanbanBoard.Services
+{
+    public class SMSoption
+    {
+        public string accountSid { get; set; }
+        public string authToken { get; set; }
+    }
+}
